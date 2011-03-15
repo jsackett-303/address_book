988200e8af966096a8865f192ac47424d0219379
jsack777 <jsack777@gmail.com>
2011-03-14 22:37:13 -0500

    use inherited_resources and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AddressBook::Application.routes.draw do
+  resources :contacts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +57,7 @@ AddressBook::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  root :to => "contacts#index"
+
 end
---
+++ db/migrate/20110315022441_create_contacts.rb
@@ -0,0 +1,18 @@
+class CreateContacts < ActiveRecord::Migration
+  def self.up
+    create_table :contacts do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :postal_code
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contacts
+  end
+end
